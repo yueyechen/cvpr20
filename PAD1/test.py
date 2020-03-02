972e105e0e7ea808274b2289f90136fcc9da8fc0
jiachen.xue <bingdianyixia963@126.com>
2020-03-02 17:02:59 +0800

    delete PAD1

--- PAD1/test.py
+++
@@ -1,9 +0,0 @@
-from config import get_config
-from learner import face_learner
-
-
-if __name__ == '__main__':
-    conf = get_config(training=False)
-    learner = face_learner(conf, inference=True)
-    learner.test(conf)
-
